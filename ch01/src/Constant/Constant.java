a3e61472b00cea9c7aca671c9ec90e538796a422
okskcctv <okskcctv@gmail.com>
2022-06-22 10:44:52 +0900

    이름 수정

---
+++ ch01/src/Constant/Constant.java
@@ -0,0 +1,27 @@
+package Constant;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		// 상수 - 변하지 않는 것 대문자로 표기함
+		// 상수 선언 할땐 final 선언
+		final int MONTH = 12;
+		final int MAX_NUM = 100;
+		final int MIN_NUM = 1;
+		
+		// MONTH = 6;
+		// MAX_NUM = 1000;
+		
+		System.out.println(MONTH);
+		System.out.println(MAX_NUM);
+		
+		final double PI = 3.14;
+		int radius = 5;
+		double area = 0.0;
+		
+		area = radius * radius * PI;
+		System.out.println(area);
+		
+	}
+
+}
